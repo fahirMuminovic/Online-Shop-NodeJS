c02f242359fa98d2c571d7e68ec6f2f1b18a1aff
Fahir Muminovic <muminovicfahir998@gmail.com>
2023-01-19 16:43:02 +0100

    fixed no body tag in response object

--- 25-Creating a Node Server/app.js
+++ 25-Creating a Node Server/app.js
@@ -17,7 +17,7 @@ const server = http.createServer((req, res) => {
 	res.setHeader('Content-Type', 'text/html');
 	res.write('<html>');
 	res.write('<head><title>My First Server</title></head>');
-	res.write('<h1>Hello from my Node.js Server!</h1>');
+	res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
 	res.write('</html>');
 	res.end();
 });
