4788d21a0d871d2f4da6a0ad1c90991d18f99478
Fahir Muminovic <muminovicfahir998@gmail.com>
2023-01-19 16:58:43 +0100

    added next lecture

---
+++ 31-Routing Requests/app.js
@@ -0,0 +1,36 @@
+const http = require('http');
+
+//this function runs for every request made to the server
+/* function rqListener(req,res){
+    //funtion body
+}
+
+http.createServer(rqListener); */
+//the above code can be replaced by anonymous funcions like this:
+
+//store the server address in variable named server
+const server = http.createServer((req, res) => {
+	const url = req.url;
+
+	if (url === '/') {
+		res.write('<html>');
+		res.write('<head><title>Enter Message</title></head>');
+		res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+		res.write('</html>');
+		return res.end();
+	}
+
+	//node.js way of sending a response
+	res.setHeader('Content-Type', 'text/html');
+	res.write('<html>');
+	res.write('<head><title>My First Server</title></head>');
+	res.write('<h1>Hello from my Node.js Server!</h1>');
+	res.write('</html>');
+	res.end();
+});
+
+//listen for incoming requests made to the server
+server.listen(3000);
+
+//to stop a server we can use process.exit();
+//process.exit();
