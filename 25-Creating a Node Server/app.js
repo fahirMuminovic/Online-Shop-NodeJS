3e8fcbb3392891e398d144ee49436eabb087cbaf
Fahir Muminovic <muminovicfahir998@gmail.com>
2023-01-19 16:30:54 +0100

    played around with the request object

--- 25-Creating a Node Server/app.js
+++ 25-Creating a Node Server/app.js
@@ -10,8 +10,12 @@ http.createServer(rqListener); */
 
 //store the server address in variable named server
 const server = http.createServer((req, res) => {
-    console.log(req);
+	//console.log(req);
+	console.log(req.url, req.method, req.headers);
 });
 
 //listen for incoming requests made to the server
 server.listen(3000);
+
+//to stop a server we can use process.exit();
+//process.exit();
