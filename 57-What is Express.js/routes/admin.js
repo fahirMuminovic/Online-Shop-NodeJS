f51fbcec3067c887f1e04d39bbe0fcfa87e6be24
Fahir Muminovic <muminovicfahir998@gmail.com>
2023-01-22 00:03:13 +0100

    refactored path and dirname

--- 57-What is Express.js/routes/admin.js
+++ 57-What is Express.js/routes/admin.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const path = require('path');
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 //router is like a mini express app and holds all the routes that we specify, then we export the routes and use them in app.js
 
 router.get('/add-product', (req, res, next) => {
-	res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+	res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
--- 57-What is Express.js/routes/shop.js
+++ 57-What is Express.js/routes/shop.js
@@ -1,10 +1,11 @@
 const express = require('express');
-//used to get path of views files 
-const path = require('path'); 
+//used to get path of views files
+const path = require('path');
+const rootDir = require('../util/path');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-	res.sendFile(path.join(__dirname,'../', 'views', 'shop.html'));
+	res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
