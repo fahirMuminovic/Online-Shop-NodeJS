edb124cc1e97fe9145be57ef33d07d9ecd028066
Fahir Muminovic <muminovicfahir998@gmail.com>
2023-01-23 21:06:34 +0100

    added a public folder and moved the css files to it

--- 57-What is Express.js/app.js
+++ 57-What is Express.js/app.js
@@ -6,22 +6,23 @@ const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const exp = require('constants');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //the order matters when we use router.use in the router files
 //but if we use router.get or post or any other
 //then express uses exact matching for the routes
-app.use('/admin',adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 //catch-all middleware
-app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+app.use((req, res, next) => {
+	res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 //shorter way of starting the server using express
-app.listen(3000, ()=>{
-    console.log('App started on http://localhost:3000');
+app.listen(3000, () => {
+	console.log('App started on http://localhost:3000');
 });
-
