a1c16bb3ecadf3ac83ea1ec48144c395d89f459d
Fahir Muminovic <muminovicfahir998@gmail.com>
2023-01-24 23:19:56 +0100

    added a shop.pug file and started using templating engines and files

--- Working with Dynamic Content  Adding Templating Engines/routes/shop.js
+++ Working with Dynamic Content  Adding Templating Engines/routes/shop.js
@@ -8,8 +8,11 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  console.log(adminData.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+	/* console.log(adminData.products);
+  res.sendFile(path.join(rootDir, 'views', 'shop.html')); */
+	//use the shop.pug template for this get request
+	res.render('shop');
 });
 
 module.exports = router;
+
