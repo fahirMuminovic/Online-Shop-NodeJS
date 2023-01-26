fc959f3df1e0c09eff39249abc4910cb7f52ab18
Fahir Muminovic <muminovicfahir998@gmail.com>
2023-01-26 20:46:28 +0100

    added files for next lecture

---
+++ Working with Dynamic Content  Adding Templating Engines - EJS/routes/shop.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+	//use the data that an admin has entered in the form on /admin/add-product
+	const products = adminData.products;
+	//use the shop.pug template for this get request
+	//as the second argument to .render we can send data as an key value pare, also it is possible to send multiple key value pares contaning different data
+	res.render('shop', {
+		prods: products,
+		pageTitle: 'Shop',
+		path: '/',
+		hasProducts: products.length > 0,
+		activeShop: true,
+		productCSS: true,
+	});
+});
+
+module.exports = router;
+
