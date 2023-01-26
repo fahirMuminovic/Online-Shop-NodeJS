436b179b0bbef880796099ceb70bc7ad941a32a0
Fahir Muminovic <muminovicfahir998@gmail.com>
2023-01-26 15:48:48 +0100

    started converting from pug to handlebars

--- Working with Dynamic Content  Adding Templating Engines - Handlebars/app.js
+++ Working with Dynamic Content  Adding Templating Engines - Handlebars/app.js
@@ -2,11 +2,14 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const engine = require('express-handlebars');
 
 const app = express();
 //define the default templating engine
-app.set('view engine', 'pug');
-app.set('views', 'views');
+//for handlebars we need to manually set the views engine
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -18,8 +21,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-	//res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-	res.status(404).render('404', {pageTitle: 'Page Not Found'});
+	res.status(404).render('404', { pageTitle: 'Page Not Found!' });
 });
 
 app.listen(3000, () => {
