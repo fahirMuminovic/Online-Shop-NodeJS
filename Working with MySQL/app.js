171e4c8ebef7b0e954c83fdd5e84e9262a4a3777
Fahir Muminovic <muminovicfahir998@gmail.com>
2023-02-03 00:59:51 +0100

    added new tables to db and set-up relations between them

--- Working with MySQL/app.js
+++ Working with MySQL/app.js
@@ -6,6 +6,8 @@ const sequelize = require('./util/database');
 
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 //define the default templating engine
@@ -36,10 +38,14 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 //makes tables in the db from models
 sequelize
-	.sync()
+	.sync({ force: true })
 	.then((result) => {
 		//creating a placeholder user for now
 		return User.findByPk(1);
