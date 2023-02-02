0a9f8890b76240c90aeb6f833909a52866adbaa1
Fahir Muminovic <muminovicfahir998@gmail.com>
2023-02-03 00:33:21 +0100

    added a temporary user to test app functionalities

--- Working with MySQL/app.js
+++ Working with MySQL/app.js
@@ -17,6 +17,15 @@ const errorController = require('./controllers/error');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+app.use((req, res, next) => {
+	User.findByPk(1)
+		.then((user) => {
+			req.user = user;
+			next();
+		})
+		.catch((err) => console.log(err));
+});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -32,8 +41,19 @@ User.hasMany(Product);
 sequelize
 	.sync()
 	.then((result) => {
+		//creating a placeholder user for now
+		return User.findByPk(1);
+	})
+	.then((user) => {
+		//this is only temporary to test the app
+		if (!user) {
+			return User.create({ name: 'Fahir', email: 'muminovicfahir998@gmail.com' });
+		}
+		return Promise.resolve(user);
+	})
+	.then((user) => {
+		//console.log(user);
 		app.listen(3000, () => {
-			//console.log(result);
 			console.log('App started on http://localhost:3000/');
 		});
 	})
