322010ad7207449287155335f1b281043e531681
Fahir Muminovic <muminovicfahir998@gmail.com>
2023-02-03 14:32:16 +0100

    implemented adding products to cart

--- Working with MySQL/controllers/shop.js
+++ Working with MySQL/controllers/shop.js
@@ -63,11 +63,42 @@ exports.getCart = (req, res, next) => {
 };
 
 exports.postCart = (req, res, next) => {
+	// const prodId = req.body.productId;
+	// Product.findById(prodId, (product) => {
+	// 	Cart.addProduct(prodId, product.price);
+	// });
+	// res.redirect('/cart');
 	const prodId = req.body.productId;
-	Product.findById(prodId, (product) => {
-		Cart.addProduct(prodId, product.price);
-	});
-	res.redirect('/cart');
+	let fetchedCart;
+	req.user
+		.getCart()
+		.then((cart) => {
+			fetchedCart = cart;
+			return cart.getProducts({ where: { id: prodId } });
+		})
+		.then((cartItems) => {
+			let cartItem;
+			if (cartItems.length > 0) {
+				cartItem = cartItems[0];
+			}
+
+			let newQuantity = 1;
+			if (cartItem) {
+				const oldQuantity = cartItem.cartItem.quantity;
+				newQuantity = oldQuantity + 1;
+				return fetchedCart.addProduct(cartItem, { through: { quantity: newQuantity } });
+			} else {
+				return Product.findByPk(prodId)
+					.then((product) => {
+						return fetchedCart.addProduct(product, { through: { quantity: newQuantity } });
+					})
+					.catch((err) => console.log(err));
+			}
+		})
+		.then(() => {
+			res.redirect('/cart');
+		})
+		.catch((err) => console.log(err));
 };
 
 exports.postDeleteCartItem = (req, res, next) => {
