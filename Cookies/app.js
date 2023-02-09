af475879ba4ea64dce4b69ffa11ec35b7e126718
Fahir Muminovic <muminovicfahir998@gmail.com>
2023-02-09 12:20:27 +0100

    added a login form and view render

--- Cookies/app.js
+++ Cookies/app.js
@@ -15,6 +15,7 @@ const errorController = require('./controllers/error');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -30,6 +31,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
