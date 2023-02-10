88123803a2cde717ffa2d590330e0a7adfa39472
Fahir Muminovic <muminovicfahir998@gmail.com>
2023-02-10 16:35:03 +0100

    added sessions and cookies

--- Cookies/controllers/admin.js
+++ Cookies/controllers/admin.js
@@ -7,7 +7,7 @@ exports.getProducts = (req, res, next) => {
 				prods: products,
 				pageTitle: 'Admin Products',
 				path: '/admin/products',
-				isAuthenticated: req.isLoggedIn,
+				isAuthenticated: req.session.isLoggedIn,
 			});
 		})
 		.catch((err) => console.log(err));
@@ -18,7 +18,7 @@ exports.getAddProduct = (req, res, next) => {
 		pageTitle: 'Add Product',
 		path: '/admin/add-product',
 		editMode: false,
-		isAuthenticated: req.isLoggedIn,
+		isAuthenticated: req.session.isLoggedIn,
 	});
 };
 
@@ -67,7 +67,7 @@ exports.getEditProduct = (req, res, next) => {
 				path: '/admin/edit-product',
 				editMode: isInEditMode,
 				product: product,
-				isAuthenticated: req.isLoggedIn,
+				isAuthenticated: req.session.isLoggedIn,
 			});
 		})
 		.catch((err) => {
--- Cookies/controllers/auth.js
+++ Cookies/controllers/auth.js
@@ -1,13 +1,30 @@
+const User = require("../models/user");
+
 exports.getLogin = (req, res, next) => {
-	const isLoggedIn = req.get('Cookie').trim().split('=')[1];
-	res.render('auth/login', {
-		path: '/login',
-		pageTitle: 'Login',
-		isAuthenticated: isLoggedIn,
-	});
+  const isLoggedIn = req.session.isLoggedIn;
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: isLoggedIn,
+  });
 };
 
 exports.postLogin = (req, res, next) => {
-	res.setHeader('Set-Cookie', 'loggedIn=true');
-	res.redirect('/');
+  User.findById("63e27db2867f89580d025302")
+    .then((user) => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save((err) => {
+        console.log(err);
+        res.redirect("/");
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect("/");
+  });
 };
